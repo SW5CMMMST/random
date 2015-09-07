9185f62970d9826c4317c8aa2bef68dfa5042bd1
Mathias Sass Michno <mmichn13@student.aau.dk>
2015-09-07 09:19:29 -0400

    emoji and joke

--- quote.sh
+++ quote.sh
@@ -2,4 +2,4 @@
 json="$(curl -s 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')"
 quote="$(echo $json | jq '.quoteText' | tr -d '"')"
 person="$(echo $json | jq '.quoteAuthor' | tr -d '"')"
-slackcli -h random -u "$person" -m "$quote" 
+slackcli -h bot -u "$person" -m "$quote" 
