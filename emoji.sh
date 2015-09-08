06636846ed6decaa03c727c78f3a9f5ae9096bbe
Mathias Sass Michno <mathiassmichno@gmail.com>
2015-09-08 14:28:51 -0400

    fixes relative path in emoji

--- emoji.sh
+++ emoji.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-emoji=$(shuf -n 1 ./emojilist.txt | tr -d '\n' | tr -d '\r')
-manyEmojies=$(shuf -n 20 ./emojilist.txt | tr '\n' '  ' | tr -d '\r')
+emoji=$(shuf -n 1 $(dirname $0)/emojilist.txt | tr -d '\n' | tr -d '\r')
+manyEmojies=$(shuf -n 20 $(dirname $0)/emojilist.txt | tr '\n' '  ' | tr -d '\r')
 slackcli -h bot -u "Emoji Man" -e "$emoji" -m "$manyEmojies" 
